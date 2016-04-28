62ada945669dc1f96c75976642bb4dab13be96a2
yuya-m-ms <maemichi@monosense.co.jp>
2016-04-28 17:43:07 +0900

    Add index view; Show in port 80

--- app.rb
+++ app.rb
@@ -1,5 +1,10 @@
 %w[sinatra sinatra/reloader].each(&method(:require))
 
+set :environment, :production
+set :port, 80
+
 get '/' do
-    'Hello, Sinatra!'
+    @title = 'Hackathon'
+    @text = 'モノセンス合宿2016年4月'
+    erb :index
 end
