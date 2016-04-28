4de844f0ad3d760e59fbc193d9d5c618fe0faf4a
yuya-m-ms <maemichi@monosense.co.jp>
2016-04-29 07:43:57 +0900

    Try Fix URI

--- app.rb
+++ app.rb
@@ -1,8 +1,5 @@
 %w[sinatra sinatra/reloader redis json net/http].each(&method(:require))
 
-gcmuri = 'https://android.googleapis.com/gcm/send'
-apikey = 'AIzaSyBkesBWycrfZIBjivDrXqk3WNvvw1sV52U'
-
 # Redis To Go
 if ENV["REDISTOGO_URL"].nil?
     redis = Redis.new
@@ -65,12 +62,17 @@ put '/api/v0/push/message' do
 end
 
 def gcm_push(ids)
-    uri = URI.parse(gcmuri)
-    req = Net::HTTP::Post.new(uri.path, initheader = {'Authorization' => "key=#{apikey}"})
+    gcm_uri = 'https://android.googleapis.com/gcm/send'
+    api_key = 'AIzaSyBkesBWycrfZIBjivDrXqk3WNvvw1sV52U'
+
+    uri = URI.parse(gcm_uri)
+    req = Net::HTTP::Post.new(uri.path, initheader = {'Authorization' => "key=#{api_key}"})
     req.body = {registration_ids:ids, collapse_key:1, 'data.message' => 'posted from gcm'}.to_json
+
     https = Net::HTTP.new(uri.host, uri.port)
     https.use_ssl = true
     https.set_debug_output $stderr
+
     res = https.request(req)
     res.body
 end
