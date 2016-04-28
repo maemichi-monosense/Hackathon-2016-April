b23d6bb8bbf0d47c8fbe845af317be3c667b7129
yuya-m-ms <maemichi@monosense.co.jp>
2016-04-29 03:37:48 +0900

    Add Redis To Go support

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
-%w[sinatra sinatra/reloader].each(&method(:require))
+%w[sinatra sinatra/reloader redis].each(&method(:require))
+
+# Redis To Go
+if ENV["REDISTOGO_URL"] != nil
+    uri = URI.parse(ENV["REDISTOGO_URL"])
+    redis = Redis.new host:uri.host, port:uri.port, password:uri.password
+else
+    redis = Redis.new host:'localhost', port:6379
+end
 
 get '/' do
     # @title = 'Hackathon'
