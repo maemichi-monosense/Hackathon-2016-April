c6840edccab429d053073cae5174ed02c70a4629
Yuya Ma'emichi <yuya-m-ms@users.noreply.github.com>
2016-04-29 11:37:12 +0900

    Refactoring (#12)
    * Prettify JSON
    
    * Remove no use
    
    * Fix & Add http status
    
    * Fix JSON

--- app.rb
+++ app.rb
@@ -9,36 +9,15 @@ else
 end
 
 get '/' do
-    # @title = 'Hackathon'
-    # @text = 'モノセンス合宿2016年4月'
     html :index
 end
 
 def html(view)
     File.read(File.join('public', "#{view.to_s}.html"))
-    # send_file File.join(settings.public_folder, "#{view.to_s}.html")
-end
-
-get '/pwd' do
-    Dir.pwd
-end
-
-get '/redis' do
-    last_access_at = nil # init
-    last_access_at = redis.get 'LAST_ACCESS_AT'
-    last_access_at ||= Time.now
-    redis.set('LAST_ACCESS_AT', Time.now)
-    "@#{ENV["REDISTOGO_URL"].nil? ? 'Local' : 'Heroku'}
-    <br>
-    #{ENV.to_a.to_s}
-    <br>
-    Last access at: #{last_access_at.to_s}
-    <br>
-    Current access at: #{Time.now.to_s}"
 end
 
 get '/api/v0/id/set' do
-    redis.smembers('id-set').to_json
+    JSON.pretty_generate redis.smembers('id-set')
 end
 
 put '/api/v0/id/registration' do
@@ -50,7 +29,7 @@ put '/api/v0/id/registration' do
 end
 
 get '/api/v0/push/message' do
-    {title:redis.get('push.title'), text:redis.get('push.text')}.to_json
+    JSON.pretty_generate({title:redis.get('push.title'), text:redis.get('push.text')})
 end
 
 put '/api/v0/push/message' do
@@ -85,6 +64,7 @@ end
 post '/api/v0/push/message' do
     ids = redis.smembers 'id-set'
     gcm_push ids
+    status 201
 end
 
 get '/management' do
